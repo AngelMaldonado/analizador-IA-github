1a2d55c4ef7934bde63865ad9f13624363410ff8
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-02 20:19:22 -0600

    Primer acercamiento a la validación del usuario y API key de Gemini

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
+
 import Input from "@/components/Input";
+import { createGoogleGenerativeAI, google } from '@ai-sdk/google'
+import { generateText } from "ai"
 import { FaGithubAlt, FaGoogle } from "react-icons/fa";
-import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react";
 import "./app.css"
-import { getGithubUser } from "@/lib/ai-reviewer";
 
 export default function Home() {
-  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [apiKeyError, setApiKeyError] = useState('')
+  const [userError, setUserError] = useState('')
 
   return (
     <main className="flex-column">
@@ -16,15 +20,50 @@ export default function Home() {
         <h3>
           Escribe un perfil de GitHub y recibe retroalimentación sobre el desarrollador.
         </h3>
-        <form onSubmit={getGithubUser} className="flex-column">
-          <Input required icon={<FaGoogle />} placeholder={"Google Gemini API key"} title={"API key de Google Gemini"} type={"password"} name={"apikey"} id={"apikey"} />
-          <Input required icon={<FaGithubAlt />} placeholder={"Ingresa el usuario de GitHub"} title={"Usuario de GitHub"} type={"text"} name={"user"} id={"user"} />
+        <form onSubmit={handleSubmit} className="flex-column">
+          <Input required icon={<FaGoogle />} error={apiKeyError} placeholder={"Google Gemini API key"} title={"API key de Google Gemini"} type={"password"} name={"apikey"} id={"apikey"} />
+          <Input required icon={<FaGithubAlt />} error={userError} placeholder={"Ingresa el usuario de GitHub"} title={"Usuario de GitHub"} type={"text"} name={"user"} id={"user"} />
           <button type="submit">
             <span>🔎</span>
-            Revisar
+            {loading ? "Cargando..." : "Revisar"}
           </button>
         </form>
       </section>
     </main>
   );
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    setLoading(true)
+    event.preventDefault()
+
+    const user = new FormData(event.target as HTMLFormElement).get("user") as string
+    const apiKey = new FormData(event.target as HTMLFormElement).get("apikey") as string
+    const provider = createGoogleGenerativeAI({ apiKey: apiKey })
+    let text = ""
+
+    try {
+      const res = await fetch(`https://api.github.com/users/${user}`)
+
+      if (res.status === 200)
+        // Redirigir a la página de resultados
+        console.log(await res.json())
+      else if (res.status === 404) {
+        setUserError("Usuario no encontrado")
+        setTimeout(() => setUserError(""), 3000)
+      }
+      else {
+        setUserError("Error desconocido con Github")
+        setTimeout(() => setUserError(""), 3000)
+      }
+
+      text = (await generateText({ model: provider("models/gemini-1.5-flash-latest"), prompt: "Just answer Ok" })).text
+    } catch (_) {
+      if (text.length === 0) {
+        setApiKeyError("API key inválida")
+        setTimeout(() => setApiKeyError(""), 3000)
+      }
+    }
+
+    setLoading(false)
+  }
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -3,7 +3,7 @@ import "./Input.css"
 
 export type InputProps = {
   required?: boolean,
-  onEnter?: (args: any) => void,
+  error?: string,
   icon: React.ReactElement,
   placeholder: string,
   title: string,
@@ -13,18 +13,13 @@ export type InputProps = {
 }
 
 export default function Input(props: InputProps) {
-  const { icon, onEnter, ...inputProps } = props
+  const { icon, error, ...inputProps } = props
 
   return (
     <div className="input-wrapper">
-      <input {...inputProps}
-        onKeyDown={(e) => {
-          if (onEnter && e.key === "Enter") {
-            onEnter((e.target as HTMLInputElement).value)
-          }
-        }}
-      />
+      <input {...inputProps} />
       {icon}
+      <span aria-hidden={!error} hidden={!error}>{error}</span>
     </div>
   )
 }
