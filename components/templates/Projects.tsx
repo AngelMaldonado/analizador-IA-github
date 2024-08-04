ea8f607cd2c4369e6c02bff0b48fec3e51fb9095
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-04 10:45:03 -0600

    Estilizando plantilla de proyectos

--- components/templates/Projects.tsx
+++ components/templates/Projects.tsx
@@ -1,4 +1,5 @@
 import { Project } from "@/lib/models/Project"
+import "./Projects.css"
 
 type ProjectsProps = {
   projects: Project[]
@@ -6,20 +7,25 @@ type ProjectsProps = {
 
 export default function Projects(props: ProjectsProps) {
   return (
-    <ul>
-      {props.projects.map((project, index) =>
-        <li key={`project-${index}`}>
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-          <ul>
-            {project.languages.map((language, index) =>
-              <li key={`language-${index}`}>{language}</li>
-            )}
-          </ul>
-          <p>{project.stars} ⭐</p>
-          <p>{project.forks} 🍴</p>
-        </li>
-      )}
-    </ul>
+    <div>
+      <h2>Proyectos</h2>
+      <ul className="projects">
+        {props.projects.map((project, index) =>
+          <li key={`project-${index}`}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <div className="stats">
+              <p className="stars">{project.stars} Estrellas✨</p>
+              <p className="forks">{project.forks} Forks🍴</p>
+            </div>
+            <ul>
+              {project.languages.map((language, index) =>
+                <li key={`language-${index}`}>{language}</li>
+              )}
+            </ul>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
